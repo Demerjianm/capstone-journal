852782c26687c6bc1a90946bf6f036acc5b37067
Michael Demerjian <michaeldemerjian@gmail.com>
2017-06-01 20:27:11 -0600

    the beginning of the end

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Auth from './components/Auth';
+import Journal from './components/Journal'
+
+const App = () => (
+  <div>
+    <NavBar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/register" render={ (props) => <Auth {...props} title="Register" /> } />
+      <Route path="/login" render={ (props) => <Auth {...props} title="Login" /> } />
+    </Switch>
+  </div>
+);
+
+export default App;
