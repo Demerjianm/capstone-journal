70e75b13becf52ac9db401e8df9525920152117a
Michael Demerjian <michaeldemerjian@gmail.com>
2017-06-06 22:29:33 -0600

    some changes to make run on heroku and locally

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose');
-const dbUrl = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/capstone-journal-dpl'
+const dbUrl = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/journal'
 mongoose.connect(dbUrl);
 
 const app = express();
