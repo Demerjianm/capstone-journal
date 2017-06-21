b0d7bcbffdda3da3b01482a4f4ac439bcd660a7a
Shannon Byland <shannonbyland@gmail.com>
2017-06-20 20:12:24 -0600

    slideshow now rendering, need to fix black bar on bottom

--- client/src/components/SlideShow.js
+++ client/src/components/SlideShow.js
@@ -1,35 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getFiveEntries } from '../actions/slideshow';
-import { Carousel } from 'nuka-carousel';
+import { Carousel } from 'react-responsive-carousel';
 import { Header } from 'semantic-ui-react';
+import 'react-responsive-carousel/lib/styles/main.css';
+import 'react-responsive-carousel/lib/styles/carousel.css';
 
 class SlideShow extends Component {
-
   componentDidMount() {
     this.props.dispatch(getFiveEntries());
   }
 
-//  mixins: [Carousel.ControllerMixin]
-
-// displayTopFive = () => {
-//   return this.props.entries.map( entry => {
-//     return (
-//         <img src={entry.image}/>
-//       )
-//   })
-// }
-
   render() {
-    console.log(this.props.topFive);
+    const renderJournals = this.props.topFive.map(journal => {
+      return (
+        <div>
+          <img src={journal.image} />
+          <p className="legend">{journal.title}</p>
+        </div>
+      )
+    })
+
     return (
-      <Carousel ref="carousel" data={this.setCarouselData.bind(this, 'carousel')}>
-        <img src="http://res.cloudinary.com/journal-love/image/upload/v1497576672/fnou9dw9mzwfildhsax1.jpg"/>
-        <img src="http://res.cloudinary.com/journal-love/image/upload/v1497576672/fnou9dw9mzwfildhsax1.jpg"/>
-        <img src="http://res.cloudinary.com/journal-love/image/upload/v1497576672/fnou9dw9mzwfildhsax1.jpg"/>
-        <img src="http://res.cloudinary.com/journal-love/image/upload/v1497576672/fnou9dw9mzwfildhsax1.jpg"/>
-        <img src="http://res.cloudinary.com/journal-love/image/upload/v1497576672/fnou9dw9mzwfildhsax1.jpg"/>
-    </Carousel>
+      //add style to carousel - may get it override the default style sheet.
+      <Carousel axis="horizontal" showThumbs={false} showArrows={true}>
+        {renderJournals}
+     </Carousel>
     )
   }
 };
