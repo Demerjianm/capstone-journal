1ef97bc5ab36c90cd2e19b161cef710715696b42
Shannon Byland <shannonbyland@gmail.com>
2017-06-21 19:46:55 -0600

    working on image sizing

--- client/src/components/SlideShow.js
+++ client/src/components/SlideShow.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { getFiveEntries } from '../actions/slideshow';
 import { Carousel } from 'react-responsive-carousel';
 import { Header } from 'semantic-ui-react';
+import { Image, Transformation } from 'cloudinary-react';
 import 'react-responsive-carousel/lib/styles/main.css';
 import 'react-responsive-carousel/lib/styles/carousel.css';
 
@@ -15,15 +16,14 @@ class SlideShow extends Component {
     const renderJournals = this.props.topFive.map(journal => {
       return (
         <div>
-          <img src={journal.image} />
+          <Image cloudName="journal-love" publicId={journal.image} height="700" crop="crop" />
           <p className="legend">{journal.title}</p>
         </div>
       )
     })
 
     return (
-      //add style to carousel - may get it override the default style sheet.
-      <Carousel axis="horizontal" showThumbs={false} showArrows={true}>
+      <Carousel axis="horizontal" showThumbs={false} showArrows={true} width="100%">
         {renderJournals}
      </Carousel>
     )
