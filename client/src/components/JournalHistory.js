964679f18429948f5f07611cc55a308aec1bffe2
Sammie Worthen <sammworthen@gmail.com>
2017-06-24 14:21:43 -0600

    style history page

--- client/src/components/JournalHistory.js
+++ client/src/components/JournalHistory.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Header, Image, Icons, List } from 'semantic-ui-react';
+import Timestamp from 'react-timestamp';
+import { Header, Image as ImageComponent, Icon, List, Item, Label } from 'semantic-ui-react';
 import { getEntries, updateEntry, deleteEntry } from '../actions/journalentry';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -16,26 +17,44 @@ class JournalHistory extends Component {
   displayEntries = () => {
      return this.props.entry.map( ent => {
       return (
-        <li key={ent._id} className="collection-item">
-          <div className='ui list'>
-            <div className='item'>
-              <h4>Title</h4>
-                { ent.title }
-            </div>
-            <div className='item'>
-              <h5>Body</h5>
-                { ent.body }
-            </div>
-            <div className='item'>
-              <h4>Images</h4>
-                <Image src={ent.image} size='tiny' />
-            </div>
-              <div style={{ cursor: 'pointer' }}>
-                <i className="big edit icon" onClick={() => this.toggleEdit(ent._id)}></i>
-                <i className="big trash basic icon" onClick={() => this.props.dispatch(deleteEntry(ent._id))}></i>
-              </div>
-          </div>
-        </li>
+        // <li key={ent._id} className="collection-item">
+        //   <div className='ui list'>
+        //     <div className='item'>
+        //       <h4>Title</h4>
+        //         { ent.title }
+        //     </div>
+        //     <div className='item'>
+        //       <h5>Body</h5>
+        //         { ent.body }
+        //     </div>
+        //     <div className='item'>
+        //       <h4>Images</h4>
+        //         <Image src={ent.image} size='small' />
+        //     </div>
+        //       <div style={{ cursor: 'pointer' }}>
+        //         <i className="big edit icon" onClick={() => this.toggleEdit(ent._id)}></i>
+        //         <i className="big trash basic icon" onClick={() => this.props.dispatch(deleteEntry(ent._id))}></i>
+        //       </div>
+        //   </div>
+        // </li>
+
+        <Item.Group divided>
+          <Item>
+            <Item.Image size='small' src={ ent.image } />
+
+            <Item.Content>
+              <Item.Header as='a'>{ ent.title }</Item.Header>
+              <Item.Meta>
+                <Timestamp time={ ent.created_at } format="date" className='cinema' />
+              </Item.Meta>
+              <Item.Description>{ ent.body }</Item.Description>
+              <Item.Extra>
+                <Label icon='edit' size='large' onClick={() => this.toggleEdit(ent._id)} content='Edit' />
+                <Label icon='trash outline' size='large' floated='right' onClick={() => this.props.dispatch(deleteEntry(ent._id))} content='Delete' />
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
       )
     });
   }
